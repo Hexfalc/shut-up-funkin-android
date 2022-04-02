d25934bdea54b480122e289b5cd5b54abe37a697
Hex Falc The Porter <84611854+Hexfalc@users.noreply.github.com>
2022-04-02 18:49:22 +0200

    Add files via upload

---
+++ assets/preload/data/annoying-bitch/script.lua
@@ -0,0 +1,13 @@
+local originy = 0
+function onCreatePost()
+
+    setProperty('gf.visible',false)
+
+end
+function onCreate()
+
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'gameover-amy'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in mods/music/
+end
